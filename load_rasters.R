8dc504fb70cdec2156a39cc4ae452ca76ebf496f
Oluoch <K.Oluoch@cgiar.org>
2019-03-01 12:28:11 +0300

    File to create rasters in Global environment

---
+++ load_rasters.R
@@ -0,0 +1,12 @@
+# LOAD RASTERS
+
+# Load Soil nutrients information (Rasters)
+soilC <- raster('data/TZA_ORCDRC_T__M_sd1_1km.tif')
+soilPolsen <- soilC
+soilPolsen[] <- 15
+soilK <- raster('data/TZA_EXKX_T__M_xd1_1km.tif')
+soilpH <- raster('data/TZA_PHIHOX_T__M_sd1_1km.tif') / 10
+
+# Crop predictions
+WY <- raster('data/GYGAClimateZones_TZA_YW.tif') %>% resample(soilC) #'data/africa_1km_crop_TZA.tif'
+
