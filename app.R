28b6d704a2f872cf7faeb68dfc0c059aca3d2d83
Oluoch <K.Oluoch@cgiar.org>
2019-04-23 16:56:24 +0300

    The app has been reduced to two key functionalities. Getting a yield estimate for a location and getting a yield estimate for the whole of Tanzania.

--- app.R
+++ app.R
@@ -21,18 +21,6 @@ ui = dashboardPage(
       
       # The Inputs to the Model
       h2("INPUTS"),
-      #Farm Information
-      div( style = "display:inline;background-color: rgb(120, 120, 120);color:White;width:400px", #;height:120px
-           h4("1 Farm Information"),
-           numericInput(inputId = 'in11', label = "1.1 Farm Size (HA)", 
-                        value = 10, min = 0, max = 1000, step = NA)
-           #h5(".  1.2 Farm Location"),
-           # numericInput(inputId = 'in12', label = "Longitude", 
-           #              value = 10, min = 0, max = NA, step = NA),
-           # numericInput(inputId = 'in13', label = "Latitude", 
-           #              value = 10, min = 0, max = NA, step = NA)
-      ),
-      
       
       div( style = "width:400px",
            h4("2 Fertilizer Information"),
@@ -63,13 +51,26 @@ ui = dashboardPage(
            )
       ),
       
-      numericInput(inputId = 'in310', label = "Total Fertilizer Cost", 
-                   value = 10, min = 0, max = NA, step = NA),
-      
-      div( style = "color:White;width:400px", #;height:200px
-           h4("3 Maize Information"),
-           numericInput(inputId = 'in31', label = "3.1 Expected Income per KG", value = 20, min = 0, max = NA, step = NA)
+      # numericInput(inputId = 'in310', label = "Total Fertilizer Cost", 
+      #              value = 10, min = 0, max = NA, step = NA),
+      #Farm Information
+      div( style = "display:inline;background-color: rgb(120, 120, 120);color:White;width:400px", #;height:120px
+           h4("1 Farm Information"),
+           # numericInput(inputId = 'in11', label = "1.1 Farm Size (HA)", 
+           #              value = 10, min = 0, max = 1000, step = NA)
+           h5("Estimate yield in a location"),
+           numericInput(inputId = 'in12', label = "Longitude",
+                        value = 1650000, min = 1037375, max = 2247375, step = NA),
+           numericInput(inputId = 'in13', label = "Latitude",
+                        value = -1250000, min = -1841875, max = -650875, step = NA)
       ),
+      actionButton(inputId = 'run_point', label = "Get Point Estimate"),
+      
+      
+      # div( style = "color:White;width:400px", #;height:200px
+      #      h4("3 Maize Information"),
+      #      numericInput(inputId = 'in31', label = "3.1 Expected Income per KG", value = 20, min = 0, max = NA, step = NA)
+      # ),
       
       # Auto hide advanced options unless needed
       a(id = "AdvancedOptions", "4 Advanced Options"),
@@ -80,7 +81,7 @@ ui = dashboardPage(
              numericInput(inputId = "in52", label = "4.2 Advanced Option 2", value = 45, min = 0, max = NA, step = NA)
         )
       ),
-      actionButton(inputId = 'run', label = "RUN")
+      actionButton(inputId = 'run', label = "Get Country Estimate")
       
       
     )
@@ -88,11 +89,18 @@ ui = dashboardPage(
   
   # SHOW input value in the meantime
   dashboardBody(
-    # helpText('The input Values'),
-    # verbatimTextOutput('ex_out'),
-    # SHOW whole country values in the meantime
-    # plotOutput('yield_est')
-    plotOutput("farm_yield_est")
+    #verbatimTextOutput('point_yield_est'),
+    # tabBox(
+    #   # Title can include an icon
+    #   title = tagList(shiny::icon("corn"), "tabBox status"),
+    #   tabPanel("Tab1",
+    #            "Currently selected tab from first box:",
+    #            verbatimTextOutput('point_yield_est'),
+    #            infoBoxOutput('infoBox1')
+    #   )
+    # ),
+    infoBoxOutput('infoBox1'),
+    plotOutput("country_yield_est")
   )
 )
 
@@ -100,8 +108,14 @@ ui = dashboardPage(
 
 server = function(input, output, session) {
   source('QUEFTS.R')
+  source('QUEFTS_Point.R')
+  
   source('nutrientUPT.R')
+  source('nutrientUPT_Point.R')
+  
   source('combine_yields.R')
+  source('combine_yields_Point.R')
+  
   source('load_rasters.R')
   
   fert_list <- list("NPK (0.14, 0.061, 0.116)" = "NPK",
@@ -109,13 +123,8 @@ server = function(input, output, session) {
                     "Fert 3 (.180, .209, 0)" = "Fert_3"
   )
   
-  
-  
-  
   # Show/Hide advanced options
   shinyjs::onclick("AdvancedOptions", toggle(id = "advanced", anim = TRUE))
-  # Show/Hide add fertilizer options
-  shinyjs::onclick("add_fert", toggle(id = "Add_Fertilizer", anim = TRUE))
   
   # Show/Hide Fertilizer Quantity options
   observe({ toggle(id = "NPK", condition = {"NPK" %in% input$in21} )})
@@ -123,10 +132,6 @@ server = function(input, output, session) {
   observe({ toggle(id = "Fert_3", condition = {"Fert_3" %in% input$in21} )})
   
   
-  # observeEvent(input$Add, {
-  #   fert_list[paste0(input$in32, " (", input$in33, ", ", input$in34, ", ", input$in35, ")")] <-  input$in32
-  #   updateCheckboxGroupInput(session, "in31", choices = fert_list )
-  # })
   
   seeinputs <- eventReactive(input$run, {
     str(sapply(sprintf('in%d', c(11, 21:24, 27:29, 210, 31)), function(id) {
@@ -134,20 +139,58 @@ server = function(input, output, session) {
     }))
   })
   
-  # Only Respond to the RUN button
-  farm_runmodel <- eventReactive(input$run,{
-    # long <- input$in12
-    # lati <- input$in13
-    # a <- sqrt(2500 * input$in11) # Distant to edges from centre - Square farm
-    # farm.extent <- extent(long - a, long + a, lati - a, lati + a)
-    #
-    # # Extract Farm Extent
-    # farm.soilC <- raster::crop(soilC, farm.extent)
-    # farm.soilPolsen <- raster::crop(soilPolsen, farm.extent)
-    # farm.soilK <- raster::crop(soilK, farm.extent)
-    # farm.soilpH <- raster::crop(soilpH, farm.extent)
-    # farm.WY <- raster::crop(WY, farm.extent)
+  
+  ##### Point Estimates ####
+  run.pointmodel <- eventReactive(input$run_point, {
+    # Inputs
+    long <- input$in12
+    lati <- input$in13
+    a <- sqrt(2500 * input$in11) # Distant to edges from centre - Square farm
+    point.coord <- data.frame(long, lati)
+
+    ##### Extract Point SoilNutrient Values ####
+    point.soilC <- raster::extract(soilC, point.coord)
+    point.soilPolsen <- raster::extract(soilPolsen, point.coord)
+    point.soilK <- raster::extract(soilK, point.coord)
+    point.soilpH <- raster::extract(soilpH, point.coord)
+    point.WY <- raster::extract(WY, point.coord)
     
+    siteSoilNutrient <- matrix(c(point.soilC, point.soilPolsen, point.soilK, point.soilpH), ncol = 4, byrow = TRUE)
+    colnames(siteSoilNutrient) <- c('soilC', 'soilPolsen', 'soilK', 'soilpH')
+    
+    #### Calculate Point nutrients(kg/ha) Values ####
+    fert_amtNPK <- fert_amtUREA <- fert_amtFert_3 <- c(0)
+    if ("NPK" %in% input$in21){
+      fert_amtNPK <- c(input$in27)
+    }
+    if ("UREA" %in% input$in21){
+      fert_amtUREA <- c(input$in28)
+    }
+    if ("Fert_3" %in% input$in21){
+      fert_amtFert_3 <- c(input$in29)
+    }
+    fert_amt <-  matrix(c(fert_amtNPK, fert_amtUREA, fert_amtFert_3), ncol= 3)
+    #Mass fraction for NPK (0.14, 0.061, 0.116) and Urea (0.46, 0.0, 0.0)
+    fert_massfrac <-  matrix(c(0.14, 0.061, 0.116, 0.46, 0.0, 0.0, .180, .209, 0), ncol = 3, byrow = TRUE) 
+    nutrients_kg.ha <- fert_amt %*% t(fert_massfrac)
+    
+    ad <- matrix(c(26, 180, 24, 60, 540, 96)) #from Sattari 2014
+    
+    
+    QUEFTS_point(siteSoilNutrient = siteSoilNutrient,
+                 nutrients_kg.ha = nutrients_kg.ha,
+                 ad = ad
+                 )
+     
+    #siteSoilNutrient[,1]
+    #colnames(siteSoilNutrient)
+    #siteSoilNutrient[,'soilC']
+    
+  })
+
+  
+  ##### Country Estimates Model ####
+  run.countrymodel <- eventReactive(input$run,{
     # Load the data
     rasters_input <-
       sapply(list(soilC, soilPolsen, soilK, soilpH, WY), getValues) %>% cbind(index =
@@ -197,54 +240,30 @@ server = function(input, output, session) {
     results  <- soilC
     results[] <- NA
     results[rasters_input[, 'index']] <- yields
-    #plot(yields)
-    plot(results, main = "Predicted Yield")
-    #plot(soilC)
+    
+    plot(TZA_boundary, main = "Predicted Yield")
+    plot(results, add=TRUE)
+    # Add Map Scale
+    #maps::map.scale(x=1100000, y=-1700000, ratio=FALSE, metric = TRUE) 
+    # Add North arrow
+    GISTools::north.arrow(xb=2100000, yb=-800000, len=50000, lab="N", col='black')
+    
   })
   
+  ##### Outputs ####
+  #output$ex_out <- renderPrint({seeinputs()})
+  output$infoBox1 <- renderInfoBox({
+    long <- input$in12
+    lati <- input$in13
+    x <- run.pointmodel()
+    color <- 'green'
+    title1 <- paste0("Estimated Prediction for Co-ordinates - Long:", long, ",  Lati: ", lati )
+    infoBox(value = x, title = title1 , color = color,  width = 4)
+  })
+  output$country_yield_est <- renderPlot({run.countrymodel()})
+  output$point_yield_est <- renderText({run.pointmodel()})
   
-  output$ex_out <- renderPrint({seeinputs()})
-  output$farm_yield_est <- renderPlot({farm_runmodel()})
   
-  
-  # output$yield_est <- renderPlot({
-  #   # Load the data
-  #   rasters_input <-
-  #     sapply(list(soilC, soilPolsen, soilK, soilpH, WY), getValues) %>% cbind(index =
-  #                                                                               1:(nrow(.)))
-  # 
-  #   rasters_input <-
-  #     rasters_input[complete.cases(rasters_input), ]  #compute only values with all data
-  #   colnames(rasters_input) <-
-  #     c('soilC', 'soilPolsen', 'soilK', 'soilpH', 'WY', 'index')
-  #   
-  #   fert_amtNPK <- fert_amtUREA <- fert_amtFert_3 <- c(15)
-  #   
-  #   fert_massfracNPK <- c(0.14, 0.061, 0.116)
-  #   fert_massfracUREA <- c(0.46, 0.0, 0.0)
-  #   fert_massfracFert_3 <- c(.180, .209, 0)
-  #   
-  #   nutrients_kg.ha <- fert_amtNPK * fert_massfracNPK + 
-  #     fert_amtUREA * fert_massfracUREA  + 
-  #     fert_amtFert_3 * fert_massfracFert_3
-  #   
-  #   yields <-
-  #     apply(
-  #       rasters_input,
-  #       FUN = QUEFTS,
-  #       MARGIN = 1,
-  #       nutrients_kg.ha = nutrients_kg.ha
-  #     )
-  #   
-  #   # Plot the results - after converting to raster
-  #   results  <- soilC
-  #   results[] <- NA
-  #   results[rasters_input[, 'index']] <- yields
-  #   #plot(yields)
-  #   #plot(soilC)
-  #   plot(results)
-  #   }
-  #   )
 }
 
 
