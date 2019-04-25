f219142b0835a478d95c3b962bf1d04a7984cf74
Oluoch <K.Oluoch@cgiar.org>
2019-04-25 12:28:19 +0300

    Switch to from 1km rasters to 5km for speed

--- load_rasters.R
+++ load_rasters.R
@@ -1,14 +1,14 @@
 # LOAD RASTERS
 
 # Load Soil nutrients information (Rasters)
-soilC <- raster('data/TZA_ORCDRC_T__M_sd1_1km.tif')
+soilC <- raster('data2/soilC5.tif') #('data/TZA_ORCDRC_T__M_sd1_1km.tif')
 soilPolsen <- soilC
 soilPolsen[] <- 15
-soilK <- raster('data/TZA_EXKX_T__M_xd1_1km.tif')
-soilpH <- raster('data/TZA_PHIHOX_T__M_sd1_1km.tif') / 10
+soilK <- raster('data2/soilK5.tif') #('data/TZA_EXKX_T__M_xd1_1km.tif')
+soilpH <- raster('data2/soilpH5.tif') #('data/TZA_PHIHOX_T__M_sd1_1km.tif') / 10
 
 # Crop predictions
-WY <- raster('data/GYGAClimateZones_TZA_YW.tif') %>% resample(soilC) #'data/africa_1km_crop_TZA.tif'
+WY <- raster('data2/WY5.tif') #('data/GYGAClimateZones_TZA_YW.tif') %>% resample(soilC) #'data/africa_1km_crop_TZA.tif'
 
 # Boundary Raster
-TZA_boundary <- shapefile('D:/Sebastian/QUEFTS/data/laea_TZA.shp')
+TZA_boundary <- shapefile('data/laea_TZA.shp')
